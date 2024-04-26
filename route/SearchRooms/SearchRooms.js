59924bfd1a0bf4b7239c32e346f46c22c1bbbea1
AH-Ratul <ahratul21@gmail.com>
2024-04-26 22:56:41 +0600

    minor update

--- route/SearchRooms/SearchRooms.js
+++ route/SearchRooms/SearchRooms.js
@@ -2,8 +2,12 @@ const express = require("express");
 const db = require("../../DB/db");
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.send('response')
+})
+
 try {
-  router.get("/", (req, res) => {
+  router.post("/", (req, res) => {
     const { check_in, check_out } = req.body;
 
     const sql = `SELECT *
@@ -24,6 +28,7 @@ try {
                 res.status(500).json({error: 'Internal Server Error'})
             } else {
                 res.status(200).json(results);
+                //console.log(results)
             }
         } )
   });
