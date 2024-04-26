22da1f53e4360b91f2329809c654fd0e3e354e3b
AH-Ratul <ahratul21@gmail.com>
2024-04-26 23:12:41 +0600

    some changes

--- route/SearchRooms/SearchRooms.js
+++ route/SearchRooms/SearchRooms.js
@@ -2,9 +2,9 @@ const express = require("express");
 const db = require("../../DB/db");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('response')
-})
+router.get("/", (req, res) => {
+  res.send("response");
+});
 
 try {
   router.post("/", (req, res) => {
@@ -17,23 +17,30 @@ try {
           FROM booking
           WHERE (check_in BETWEEN ? AND ?)
              OR (check_out BETWEEN ? AND ?)
-             OR (check_in <= ? AND check_out >= ?)
+             OR (? <= check_in AND ? >= check_out)
         )`;
 
-        const params = [check_in, check_out, check_in, check_out, check_in, check_out]
+    const params = [
+      check_in,
+      check_out,
+      check_in,
+      check_out,
+      check_in,
+      check_out,
+    ];
 
-        db.query(sql, params, (err, results) => {
-            if(err) {
-                console.log('error', err);
-                res.status(500).json({error: 'Internal Server Error'})
-            } else {
-                res.status(200).json(results);
-                //console.log(results)
-            }
-        } )
+    db.query(sql, params, (err, results) => {
+      if (err) {
+        console.log("error", err);
+        res.status(500).json({ error: "Internal Server Error" });
+      } else {
+        res.status(200).json(results);
+        //console.log(results)
+      }
+    });
   });
 } catch (error) {
-    console.log(error)
+  console.log(error);
 }
 
 module.exports = router;
