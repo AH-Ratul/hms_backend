4db65f6578cb93c73fb4932361da1631724ac176
AH-Ratul <ahratul21@gmail.com>
2024-04-28 01:38:35 +0600

    some changes

--- route/Booking/getBookingDetails.js
+++ route/Booking/getBookingDetails.js
@@ -1,21 +1,20 @@
-const express = require('express');
+const express = require("express");
 const db = require("../../DB/db");
 const router = express.Router();
 
 try {
-    router.get("/", (req, res) => {
-      const sql = `SELECT * FROM booking`;
-      db.query(sql, (err, results) => {
-        if (err) {
-          console.log(err);
-        } else {
-          res.status(200).json(results);
-        }
-      });
+  router.get("/", (req, res) => {
+    const sql = `SELECT * FROM booking`;
+    db.query(sql, (err, results) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(200).json(results);
+      }
     });
-  } catch (error) {
-    console.log(error);
-  }
-  
-  module.exports = router;
-  
+  });
+} catch (error) {
+  console.log(error);
+}
+
+module.exports = router;
