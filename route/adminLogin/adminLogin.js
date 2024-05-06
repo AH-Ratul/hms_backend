a7c8190d1effb800bcb15b61149ea9e0a35c8d3e
AH-Ratul <ahratul21@gmail.com>
2024-05-06 15:17:29 +0600

    modified

--- route/adminLogin/adminLogin.js
+++ route/adminLogin/adminLogin.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {db }= require("../../DB/db");
+const { db, db2 } = require("../../DB/db");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -9,29 +9,36 @@ router.get("/", (req, res) => {
 try {
   router.post("/", (req, res) => {
     const { admin_email, password } = req.body;
-    
+
     if (!admin_email || !password) {
-      return res.status(400).json({ error: "all fields are required" });
+      return res.status(400).json({ error: "Emai & Password are required" });
     }
 
     const sql = `SELECT * FROM admin  WHERE admin_email="${admin_email}"`;
-    db.query(sql, (err, results) => {
+    db.query(sql, (err, results1) => {
       if (err) {
         console.error("MySQL error:", err);
-        return res.status(500).json({ error: "Internal server error" });
+        return res.status(500).json({ error: "Internal server1 error" });
       }
 
-      if (results.length > 0) {
-        const admins = results[0];
+      // execute second database
+      db2.query(sql, (err, results2) => {
+        if (err) {
+          console.log("err in db2", err);
+          return res.status(500).json({ error: "Internal Server2 Error" });
+        }
 
-        if (password === admins.password) {
+        const admin1 = results1[0];
+        const admin2 = results2[0];
+
+        if (admin1 && admin1.password === password) {
+          res.status(200).json({ message: "Login Successfull" });
+        } else if (admin2 && admin2.password === password) {
           res.status(200).json({ message: "Login Successfull" });
         } else {
           res.status(401).json({ error: "Invalid Credentials" });
         }
-      } else {
-        res.status(401).json({ error: "Invalid Credentials" });
-      }
+      });
     });
   });
 } catch (error) {
